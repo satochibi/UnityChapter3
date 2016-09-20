2b6ecac6762408a40c0850e9451397505c22b7d7
池田央 <g1444062@gmail.com>
2016-09-20 16:56:27 +0900

    first commit

---
+++ Assets/RouletteController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RouletteController : MonoBehaviour {
+
+	float rotSpeed = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			this.rotSpeed = 10f;
+		}
+
+		transform.Rotate(0, 0, this.rotSpeed);
+
+		rotSpeed *= 0.96f;
+	}
+}
